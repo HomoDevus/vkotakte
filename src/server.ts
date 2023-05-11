802f5807b560e2c97a755dc691f39897f0643afe
ItisEternity <lavrovslava207@gmail.com>
2023-05-11 13:34:31 +0300

    Cors allow from everywhere

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ config()
 
 const { FRONTEND_URL } = process.env
 
-app.use(cors({ origin: FRONTEND_URL ? [FRONTEND_URL] : [] }))
+app.use(cors())
 app.use(express.json())
 app.use(
   fileUpload({
